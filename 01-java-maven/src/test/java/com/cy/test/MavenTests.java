a606e2d04d88d9e677f4f11f17da7bcb9f8cbe44
LiSheng_z <1361640418@qq.com>
2021-07-19 12:00:40 +0800

    git test

---
+++ 01-java-maven/src/test/java/com/cy/test/MavenTests.java
@@ -0,0 +1,11 @@
+package com.cy.test;
+
+import org.junit.Test;
+
+public class MavenTests {
+
+        @Test
+        public void testHello(){
+            System.out.println("hello maven");
+        }
+}
